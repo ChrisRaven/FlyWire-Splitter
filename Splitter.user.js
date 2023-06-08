5ae27ec6f5772112dc11fa1b26cd9a44883508d1
KrzysztofKruk <kr.kruk@gmail.com>
2023-06-08 23:13:08 +0200

    Fix header (@grant none)

--- Splitter.user.js
+++ Splitter.user.js
@@ -7,6 +7,7 @@
 // @match        https://ngl.flywire.ai/*
 // @match        https://edit.flywire.ai/*
 // @connect      prodv1.flywire-daf.com
+// @grant        none
 // @updateURL    https://raw.githubusercontent.com/ChrisRaven/FlyWire-Splitter/main/Splitter.user.js
 // @downloadURL  https://raw.githubusercontent.com/ChrisRaven/FlyWire-Splitter/main/Splitter.user.js
 // @homepageURL  https://github.com/ChrisRaven/FlyWire-Splitter
